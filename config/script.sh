171123eda96a062945f514d4a31ea7970b5b37fb
Jacob Michalski <jakub.michalski@alumni.univ-avignon.fr>
2022-05-19 15:02:58 +0200

    vm deployment script correction

--- config/script.sh
+++ config/script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 j=1
-k=100
+k=32
 #recuperation du nom d'utilisateur
 user=$( whoami )
 #recuperation du nom / adresse ip de l'hote
@@ -14,8 +14,8 @@ then
 	#suppression de la configuration ssh
 	: | sudo tee /home/$user/.ssh/config
 	#suppression des cles authorisees precedement
-	head -n 2 /home/$user/.ssh/authorized_keys | sudo tee /home/$user/.ssh/authorized_keys
-	head -n 2 /home/$user/.ssh/known_hosts | sudo tee /home/$user/.ssh/known_hosts
+	head -n 1 /home/$user/.ssh/authorized_keys | sudo tee /home/$user/.ssh/authorized_keys
+	head -n 1 /home/$user/.ssh/known_hosts | sudo tee /home/$user/.ssh/known_hosts
 fi
 
 #pour chaque machine a creer
@@ -24,7 +24,7 @@ do
 	#le numero de la vm
 	machNum=$((i+100))
 	#creation de la vm avec la bonne configuration
-	multipass launch impish -n PC$machNum --mem 512m --cloud-init cloud-config.yaml
+	multipass launch jammy -n PC$machNum --mem 512m --cloud-init cloud-config.yaml
 	#recuperation de l'adresse ip de la vm nouvellement cree
 	ip=$( multipass list | awk 'END{print $3;}' )
 	#ajout de son adresse ip au fichier listant toutes les adresses
@@ -33,11 +33,11 @@ do
 	#envoie d'un fichier de configuration sur la vm nouvellement cree
 	sshpass -p "admin" scp -o StrictHostKeyChecking=no config.txt ubuntu@$ip:
 	#installation des packets necessaires sur la vm et transmission de la cle ssh 
-	sshpass -p "admin" ssh ubuntu@$ip "sudo hwclock --hctosys && \
-	sleep 5 && sudo apt update && sudo apt -y install iperf3 && sudo apt -y install sshpass && \
-	sudo sysctl net.core.somaxconn=65536 \
-	sudo sysctl net.ipv4.tcp_retries1=5 && sudo sysctl net.ipv4.tcp_retries2=20 && \
-	sudo sysctl net.ipv4.tcp_syn_retries=255 && sudo sysctl net.ipv4.tcp_synack_retries=255 && \
+	sshpass -p "admin" ssh ubuntu@$ip "sudo hwclock --hctosys && sudo localedef -i fr_FR -f UTF-8 fr_FR.UTF-8 && \
+	sleep 2 && sudo apt update && sudo apt -y install iperf && sudo apt -y install sshpass && \
+	sudo sysctl net.core.somaxconn=65536 && \
+	sudo sysctl net.ipv4.tcp_retries1=10 && sudo sysctl net.ipv4.tcp_retries2=20 && \
+	sudo sysctl net.ipv4.tcp_syn_retries=127 && sudo sysctl net.ipv4.tcp_synack_retries=127 && \
 	ssh-keygen -f /home/ubuntu/.ssh/id_rsa -q -N '""' && \
 	sshpass -p 'admin' ssh-copy-id -o StrictHostKeyChecking=no $user@$host && \
 	cat config.txt | cat - /etc/ssh/ssh_config > temp && sudo mv temp /etc/ssh/ssh_config && rm config.txt"
@@ -53,7 +53,7 @@ done
 n=$((k-j))
 if [ "$j" -eq 1 ]
 then 
-	tail -n $((n+2)) /home/$user/.ssh/authorized_keys > newkeys.txt
+	tail -n $((n+1)) /home/$user/.ssh/authorized_keys > newkeys.txt
 else
 	tail -n $((n+1)) /home/$user/.ssh/authorized_keys > newkeys.txt
 fi
